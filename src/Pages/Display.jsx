e8fd2d9df960b80908a1f229488169b82e2fbd30
ayooashmit <144807097+ayooashmit@users.noreply.github.com>
2024-08-19 16:06:05 +0530

    made the outline of the entire page

--- src/Pages/Display.jsx
+++ src/Pages/Display.jsx
@@ -14,7 +14,7 @@ export default function Display() {
     <>
     <Histrory place={place} date={date} person={person} />
       <div className=" w-screen h-screen text-white bg-black">
-        <div className=" w-screen h-1/6 text-white text-5xl text-center Card-Title">Trip Blueprint</div>
+        <div className=" w-screen h-1/6 text-white text-5xl text-center Card-Title mt-5">Trip Blueprint</div>
         <div className=" w-screen h-5/6 flex flex-col-reverse md:flex-row">
           <div className="w-screen md:w-1/2 h-2/3 md:h-full  overflow-y-scroll no-scrollbar"><Plans place={place} date={date} person={person} /></div>
           <div className="w-screen md:w-1/2 h-1/3 md:h-full  "><Maps destination={place} /></div>
